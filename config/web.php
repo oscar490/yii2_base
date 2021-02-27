fc3d846eff07da2400e3af1f0f8521367c030061
Óscar Vega <oscarvegaherrera59@gmail.com>
2021-02-27 19:15:54 +0100

    Carga inicial

---
+++ config/web.php
@@ -0,0 +1,97 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'name' => 'Yii App',
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'fYmzQrSh0QW-iEMafQoTV0UY1_3ZWUgN',
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+        
+        'assetManager' => [
+            // override bundles to use CDN :
+            'bundles' => [
+                'yii\bootstrap4\BootstrapAsset' => [
+                    'sourcePath' => null,
+                    'baseUrl' => 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0',
+                    'css' => [
+                        'dist/css/bootstrap.min.css'
+                    ],
+                ],
+                'yii\bootstrap4\BootstrapPluginAsset' => [
+                    'sourcePath' => null,
+                    'baseUrl' => 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0',
+                    'js' => [
+                        'dist/js/bootstrap.min.js'
+                    ],
+                ],
+            ],
+        ],
+        
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
---
+++ views/site/welcome.php
@@ -0,0 +1,12 @@
+<?php
+
+    $this->title = 'Inicio | ' . Yii::$app->name;
+
+?>
+
+<div class="jumbotron">
+  <h1 class="display-4">¡Bienvenido al Framework de Yii para PHP!</h1>
+  <p class="lead">Incluye el Framework de Bootstrap 4</p>
+  <hr class="my-4">
+  <a class="btn btn-primary btn-lg" href="https://www.yiiframework.com/" role="button">Documentación</a>
+</div>
